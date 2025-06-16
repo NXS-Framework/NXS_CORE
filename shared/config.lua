4f3fe35707605367606959b532075aa60de106fb
Ferenzzos <147423272+Ferenzzos@users.noreply.github.com>
2025-06-16 23:50:35 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -1,1 +1,24 @@
+Config = {}
+--
+--░█████╗░░█████╗░███╗░░██╗███████╗██╗░██████╗░
+--██╔══██╗██╔══██╗████╗░██║██╔════╝██║██╔════╝░
+--██║░░╚═╝██║░░██║██╔██╗██║█████╗░░██║██║░░██╗░
+--██║░░██╗██║░░██║██║╚████║██╔══╝░░██║██║░░╚██╗
+--╚█████╔╝╚█████╔╝██║░╚███║██║░░░░░██║╚██████╔╝
+--░╚════╝░░╚════╝░╚═╝░░╚══╝╚═╝░░░░░╚═╝░╚═════╝░
+
+-- Framework settings
+Config.Framework = "standalone" -- "esx", "qbcore", or "standalone"
+-- Disconnect/connect logging
+Config.EnableLogging = true
+
+--Discord webhook logging
+Config.EnableDiscordLogs = false
+  
+-- Discord webhooks
+Config.Webhooks = {
+    PlayerConnecting = "", 
+    PlayerDisconnected = "", 
+}
+
 
