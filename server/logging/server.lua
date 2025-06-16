d03126f62da44b9b2ed3ea25601bc9fb78a6a72b
Phoenix <159592948+Muffinzo@users.noreply.github.com>
2025-06-16 21:54:45 +0200

    Delete server/logging directory

--- server/logging/server.lua
+++
@@ -1,9 +0,0 @@
-AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
-    local src = source
-    local identifiers = GetPlayerIdentifiers(src)
-    local discordID, license = nil, nil
-
-    local playerName = GetPlayerName(src)
-
-    print("\27[35m[ NXS-Core ] \27[0m Player " .. playerName .. " is connecting.")
-end)
