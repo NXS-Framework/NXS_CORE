2d5935d8d07fc1f000a78321abde473b62738b8b
Palacinka0123 <118081136+palacinka0123@users.noreply.github.com>
2025-06-16 21:40:16 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Nexus Development'
+description ' An advanced PvP Core for FiveM servers'
+version '1.0.0.'
+
+
+
+client_scripts {
+    'client.lua'
+
+    }
+
+server_scripts {
+    'server.lua'
+
+    }
+
+shared_scripts {
+    'config.lua'
+
+    }
+  
+ dependencies {
+    'nexus_anticheat',
+    'ox_inventory',
+    'ox_lib'
+}
+   
+
+ -- files {
+ --   prostě další soubory jako html, css, js, json, pro saskyče C# :D
