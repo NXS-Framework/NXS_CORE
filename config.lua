9cad8df9028ab3038991265e0a1f40d6d045fec5
Ferenzzos <147423272+Ferenzzos@users.noreply.github.com>
2025-06-16 22:46:22 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,26 @@
+
+Config = {}
+--
+--░█████╗░░█████╗░███╗░░██╗███████╗██╗░██████╗░
+--██╔══██╗██╔══██╗████╗░██║██╔════╝██║██╔════╝░
+--██║░░╚═╝██║░░██║██╔██╗██║█████╗░░██║██║░░██╗░
+--██║░░██╗██║░░██║██║╚████║██╔══╝░░██║██║░░╚██╗
+--╚█████╔╝╚█████╔╝██║░╚███║██║░░░░░██║╚██████╔╝
+--░╚════╝░░╚════╝░╚═╝░░╚══╝╚═╝░░░░░╚═╝░╚═════╝░
+
+Config = {}
+-- Framework settings
+Config.Framework = "standalone" -- "esx", "qbcore", or "standalone"
+(
+-- Disconnect/connect logging
+Config.EnableLogging = true
+
+--Discord webhook logging
+Config.EnableDiscordLogs = false
+  
+-- Discord webhooks
+Config.Webhooks = {
+    PlayerConnecting = "", 
+    PlayerDisconnected = "", 
+}
+
