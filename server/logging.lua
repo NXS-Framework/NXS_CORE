75954fc3a82589dbc9321f90b777b1ed179c5b0c
Ferenzzos <147423272+Ferenzzos@users.noreply.github.com>
2025-06-16 23:52:47 +0000

    framework checkt, id searching

--- server/logging.lua
+++ server/logging.lua
@@ -1,15 +1,49 @@
+local ID = nil
+
 if Config and Config.EnableLogging then
 
     AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
         local src = source
         local ip = GetPlayerEndpoint(src)
-        
-        
+
+
+            if Config and Config.Framework == "qb-core" then
+                local QBCore = exports['qb-core']:GetCoreObject()
+                local Player = QBCore.Functions.GetPlayer(source)
+                if Player then
+                    ID = Player.PlayerData.citizenid
+                end
+
+            elseif Config and Config.Framework == "esx" then
+                local ESX = exports['es_extended']:getSharedObject()
+                local xPlayer = ESX.GetPlayerFromId(source)
+                if xPlayer then
+                    ID = xPlayer.identifier
+                end
+
+            elseif Config and Config.Framework == "standalone" then
+                if not playerIDs then playerIDs = {} end
+                if not lastID then lastID = 0 end
+
+                if not playerIDs[source] then
+                    lastID = lastID + 1
+                    playerIDs[source] = lastID
+                end
+
+                ID = playerIDs[source]
+            end
+
+            local safeID = ID or "unknown"
+-- zatím fungujen v standalone režim
+
         print("\27[34m[ NXS-Core ] \27[0m Player " .. playerName .. " is connecting.")
-        sendToDiscord("🔵 Player", 
-    "**" .. playerName .. "** is connecting from IP: `xxx.xxx.xxx`\n" ..
-    "_If you want to see this ip, use the command_ `/ip (id) " .. playerName .. "`", 
-    3447003)
+            sendToDiscord(
+                "🔵 Player",
+                "**" .. playerName .. "** is connecting from IP: `xxx.xxx.xxx`\n" ..
+                "Identifier: `" .. safeID .. "`\n" ..
+                "_Use_ `/ip " .. safeID .. "` _to view IP._",
+                3447003
+            )
     end)
 
     AddEventHandler('playerDropped', function(reason)
@@ -26,8 +60,10 @@ else
 end    
 
 if Config and Config.Debug then
+    print("Framework", Config.Framework)
     print("Config:", Config)
     print("Config.EnableLogging:", Config.EnableLogging)
     print("sendToDiscord:", sendToDiscord)
 end
+
 
