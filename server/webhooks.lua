fe0dd13d6783ed46386260b44a49817306199b55
Ferenzzos <147423272+Ferenzzos@users.noreply.github.com>
2025-06-16 22:47:18 +0000

    .

--- server/webhooks.lua
+++ server/webhooks.lua
@@ -1,20 +1,23 @@
 if Config and Config.EnableDiscordLogs then 
-function sendToDiscord(title, message, color)
-    local payload = {
-        username = "Server Logs",
-        embeds = {
-            {
-                title = title,
-                description = message,
-                color = color,
-                footer = {
-                    text = "NXS Core • " .. os.date("%Y-%m-%d %H:%M:%S")
+    function sendToDiscord(title, message, color)
+        local payload = {
+            username = "Server Logs",
+            embeds = {
+                {
+                    title = title,
+                    description = message,
+                    color = color,
+                    footer = {
+                        text = "Nexus PvP Core • " .. os.date("%Y-%m-%d %H:%M:%S")
+                    }
                 }
             }
         }
-    }
 
-    PerformHttpRequest(Config.Webhook, function(err, text, headers) end, 'POST', json.encode(payload), {
-        ['Content-Type'] = 'application/json'
-    })
+        PerformHttpRequest(webhook, function(err, text, headers)
+            -- Volitelně můžeš lognout odpověď: print("Webhook response:", text)
+        end, 'POST', json.encode(payload), {
+            ['Content-Type'] = 'application/json'
+        })
+    end
 end
