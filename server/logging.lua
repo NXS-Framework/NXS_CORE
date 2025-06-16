21dbc529c4d68122d978b01c958e156a13a19d73
Ferenzzos <147423272+Ferenzzos@users.noreply.github.com>
2025-06-16 22:37:44 +0000

    fixed logging

--- server/logging.lua
+++ server/logging.lua
@@ -1,27 +1,29 @@
-if Config.EnableLogging = true then
-AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
-    local src = source
-    local identifiers = GetPlayerIdentifiers(src)
-    local discordID, license = nil, nil
-    local ip = GetPlayerEndpoint(src
-    print("\27[34m[ NXS-Core ] \27[0m Player " .. playerName .. " is connecting.")
-        sendToDiscord("🔵 Player ", "**" .. name .. "** is connecting from :`||" .. ip .. "||`", 3447003)
-end)
+if Config and Config.EnableLogging then
 
-AddEventHandler('playerDropped', function(reason)
-    local playerName = GetPlayerName(src)
-    reason = reason or "Unknown"
+    AddEventHandler('playerConnecting', function(playerName, setKickReason, deferrals)
+        local src = source
+        local ip = GetPlayerEndpoint(src)
+        
+        print("\27[34m[ NXS-Core ] \27[0m Player " .. playerName .. " is connecting.")
+        sendToDiscord("🔵 Player", "**" .. playerName .. "** is connecting from IP: `||" .. ip .. "||`", 3447003)
+    end)
 
-    print("\27[34m[ NXS-Core ] \27[0m Player " .. playerName .. " has disconnected. Reason: " .. reason)
-            sendToDiscord("🔴 Player ", "**" .. name .. "** has disconnected for `" .. reason .. "`", 15158332)
-end)
-end)
+    AddEventHandler('playerDropped', function(reason)
+        local src = source
+        local playerName = GetPlayerName(src)
+        reason = reason or "Unknown"
+        
+        print("\27[34m[ NXS-Core ] \27[0m Player " .. playerName .. " has disconnected. Reason: " .. reason)
+        sendToDiscord("🔴 Player", "**" .. playerName .. "** has disconnected. Reason: `" .. reason .. "`", 15158332)
+    end)
+
 else
     print("Logging disabled or Config not loaded.")
 end    
 
 if Config and Config.Debug then
     print("Config:", Config)
-    print("Config.EnableLogging:", Config and Config.EnableLogging)
+    print("Config.EnableLogging:", Config.EnableLogging)
 end
+
 
